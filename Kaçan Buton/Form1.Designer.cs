7ee079850744f943ee08b655f79059bea72e7463
Hydrhyme <160329227+Hydrhyme@users.noreply.github.com>
2024-04-25 21:25:09 +0300

    v1

---
+++ Kaçan Buton/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Kaçan_Buton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnyok = new MaterialSkin.Controls.MaterialButton();
+            this.btnevet = new MaterialSkin.Controls.MaterialButton();
+            this.SuspendLayout();
+            // 
+            // btnyok
+            // 
+            this.btnyok.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnyok.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.btnyok.Depth = 0;
+            this.btnyok.HighEmphasis = true;
+            this.btnyok.Icon = null;
+            this.btnyok.Location = new System.Drawing.Point(476, 344);
+            this.btnyok.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnyok.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnyok.Name = "btnyok";
+            this.btnyok.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.btnyok.Size = new System.Drawing.Size(97, 36);
+            this.btnyok.TabIndex = 6;
+            this.btnyok.Text = "Yookkkkk";
+            this.btnyok.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.btnyok.UseAccentColor = false;
+            this.btnyok.UseVisualStyleBackColor = true;
+            this.btnyok.Click += new System.EventHandler(this.btnyok_Click);
+            this.btnyok.MouseEnter += new System.EventHandler(this.btnyok_MouseEnter);
+            this.btnyok.MouseHover += new System.EventHandler(this.btnyok_MouseHover);
+            // 
+            // btnevet
+            // 
+            this.btnevet.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnevet.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.btnevet.Depth = 0;
+            this.btnevet.HighEmphasis = true;
+            this.btnevet.Icon = null;
+            this.btnevet.Location = new System.Drawing.Point(264, 344);
+            this.btnevet.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnevet.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnevet.Name = "btnevet";
+            this.btnevet.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.btnevet.Size = new System.Drawing.Size(74, 36);
+            this.btnevet.TabIndex = 5;
+            this.btnevet.Text = "Evettt";
+            this.btnevet.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.btnevet.UseAccentColor = false;
+            this.btnevet.UseVisualStyleBackColor = true;
+            this.btnevet.Click += new System.EventHandler(this.btnevet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(837, 650);
+            this.Controls.Add(this.btnyok);
+            this.Controls.Add(this.btnevet);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kaçma Buton!!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private MaterialSkin.Controls.MaterialButton btnyok;
+        private MaterialSkin.Controls.MaterialButton btnevet;
+    }
+}
+
---
+++ Kaçan Buton/Form1.cs
@@ -0,0 +1,69 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kaçan_Buton
+{
+    public partial class Form1 : MaterialForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        
+
+      
+        private void btnevet_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("AALLLLLLAAHIIIMMMMM GOOOOLLLL", "<3");
+            
+        }
+
+        private void btnyok_MouseHover(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int newX = random.Next(0, this.ClientSize.Width - btnyok.Width);
+            int newY = random.Next(0, this.ClientSize.Height - btnyok.Height);
+
+            btnyok.Location = new Point(newX, newY);
+           
+
+
+        }
+
+        private void btnyok_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ayıp Oluyo Ama" , "Formu Küçültmek Yasakk!!");
+        }
+
+      
+
+        private void btnyok_MouseEnter(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int newX = random.Next(0, this.ClientSize.Width - btnyok.Width);
+            int newY = random.Next(0, this.ClientSize.Height - btnyok.Height);
+            btnyok.Location = new Point(newX, newY);
+        }
+
+       
+    }
+}
